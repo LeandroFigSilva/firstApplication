bffa4261fdad52e9721f82b3a76d29adce727925
LeandroFigSilva <34476005+LeandroFigSilva@users.noreply.github.com>
2017-12-12 17:07:44 +0000

    8

--- server.js
+++ server.js
@@ -22,6 +22,13 @@ router.get('/', (req, res) => res.json({
 }));
 
 
+app.use(function(req, res, next){
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+
 app.use('/', router);
 
 
